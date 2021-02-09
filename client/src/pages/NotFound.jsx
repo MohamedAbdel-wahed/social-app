37eb9f7b2c18495c202d2b01d47ba266cca0e907
MohamedAbdel-wahed <webdeveloper.int99@gmail.com>
2021-02-09 12:55:36 +0200

    netlify.toml spa proplem

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react"
 
 function NotFound() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <h1 className="text-3xl sm:text-5xl text-gray-500 font-semibold">
-        404 Page Not Found
+        404 Page Not Found!
       </h1>
     </div>
   )
